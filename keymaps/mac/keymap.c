527036de2cd57496e2d0246a157b82720c804ef5
mhmx <mumaxpoke@gmail.com>
2025-09-28 14:53:31 +0600

    sunday update

--- keymaps/mac/keymap.c
+++ keymaps/mac/keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2025 CIDOO
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 #include "../../../lib/rdr_lib/rdr_common.h"
 
@@ -52,7 +36,7 @@ void td_case_finished(tap_dance_state_t *state, void *user_data) {
     } else {
         tap_code16(S(A(KC_LEFT))); // select word to left
         tap_code16(C(G(KC_LALT))); // change case
-        wait_ms(250);
+        wait_ms(400);
         tap_code16(KC_RIGHT);      // unselect word (right)
         tap_code16(KC_SPC);        // add space
     }
