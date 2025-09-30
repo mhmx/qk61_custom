f7799c57012f5f46c27e0b6c9c4f17c6443c8e4b
mhmx <mumaxpoke@gmail.com>
2025-09-30 15:30:55 +0600

    alt layer fix

--- keymaps/win/keymap.c
+++ keymaps/win/keymap.c
@@ -220,7 +220,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TD(TD_NUM_TAB),    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         TD(TD_WIN_CAPS),   KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,           KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,
-        KC_LCTL,           KC_LGUI,  TD(TD_ALT_LAYER),            KC_SPC,                              KC_LALT, TD(TD_WIN_LOCK),  KC_RCTL, MO(_FUNC)
+        KC_LCTL,           KC_LGUI,  TD(TD_ALT_LAYER),            KC_SPC,                              KC_RALT, TD(TD_WIN_LOCK),  KC_RCTL, MO(_FUNC)
     ),
 
     [_NUM] = LAYOUT_tkl_ansi(
@@ -249,7 +249,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ALT] = LAYOUT_tkl_ansi(
         _______,           _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______,
-        TD(TD_ALT_TAB),            ALT_F4,   CTRL_W,   _______, CTRL_R,  CTRL_T,   _______, _______, _______, _______, _______, _______, _______, _______,
+        TD(TD_ALT_TAB),    ALT_F4,   CTRL_W,   _______, CTRL_R,   CTRL_T,   _______, _______, _______, _______, _______, _______, _______, _______,
         _______,           CTRL_A,   CTRL_S,   _______, _______,  _______,  _______, _______, _______, _______, _______, _______,          _______,
         KC_CAPS,           CTRL_Z,   CTRL_X,   CTRL_C,  CTRL_V,   PST_VAL,  _______, _______, _______, _______, _______,                   _______,
         _______,           _______,  _______,                     _______,                             _______, _______,          _______, _______
