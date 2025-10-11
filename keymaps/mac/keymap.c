9cc1af4dbb881439b4887fc96f701d2df613944b
mhmx <mumaxpoke@gmail.com>
2025-10-11 20:24:16 +0600

    mac layer left alt to nav mod

--- keymaps/mac/keymap.c
+++ keymaps/mac/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(_NUM, KC_TAB),  KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         LT(_NAV, KC_CAPS), KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,           KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,
-        KC_LCTL,           KC_LALT,  KC_LGUI,                     KC_SPC,                              KC_RGUI, TD(TD_MAC_LOCK),  KC_RCTL, MO(_FUNC)
+        KC_LCTL,           KC_LALT,  KC_LGUI,                     KC_SPC,                              LT(_NAV, KC_RGUI), TD(TD_MAC_LOCK),  KC_RCTL, MO(_FUNC)
     ),
 
     [_NUM] = LAYOUT_tkl_ansi(
